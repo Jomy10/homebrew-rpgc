f19dce07ed7a779a34d6c9b4eafdb7777b54f087
Jomy10 <jonas.vbs4@gmail.com>
2022-01-25 00:39:18 +0100

    0.1.1

--- Formula/rpgc.rb
+++ Formula/rpgc.rb
@@ -1,9 +1,9 @@
 class Rpgc < Formula
 	desc "The official compiler for the RPG esoteric programming language"
 	homepage "https://github.com/jomy10/rpg-lang"
-	url "https://github.com/Jomy10/rpg-lang/releases/download/v0.1.0-2/macos-x86_64.tar.gz"
-	sha256 "22086418427f15ad471a2ef1b822c9b88cf4797fc83ada7a8e52fbadcc4f8ee1"
-	version "0.1.0"
+	url "https://github.com/Jomy10/rpg-lang/releases/download/v0.1.1/macos-x86_64.tar.gz"
+	sha256 "f653e6b48bafb9ffd479259d7457add1f7aeb292a37c0ac8f8523d1fc43accd9"
+	version "0.1.1"
 
 	def install
 		bin.install "rpgc"
